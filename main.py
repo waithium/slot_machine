d95b55b91cee883ce2a4ba5e3973cfba3789ccc6
waithium <waithium@proton.me>
2024-04-07 22:01:20 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 #slot machine project
+import random
+
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    'A' : 2,
+    'B' : 4,
+    'C' : 6,
+    'D' : 8,
+}
+
+
 
 def deposit():
     while True:
@@ -29,3 +45,35 @@ def get_number_of_lines():
             print('please enter a number!!')
             print('-' * 22)
 
+def get_bet():
+    while True:
+        bet = input(f'how much would you like to bet on each line?({MIN_BET}-{MAX_BET}): ')
+        if bet.isdigit():
+            bet =int(bet)
+            if MIN_BET > bet or MAX_BET < bet:
+                print(f"the number is out of range, the range is {MIN_BET}-{MAX_BET}")
+                continue
+            else:
+                return bet
+        else:
+            print('please enter a number!!')
+            print('-' * 22)
+
+def main():
+    while True:
+        balance = deposit()
+        lines = get_number_of_lines()
+        bet = get_bet()
+        total_bet = bet * lines
+
+        print(f"You are betting {bet}$ on {lines} lines. Your total bet is equal to {total_bet}. \n")
+        if total_bet > balance:
+            print("you don't have enough money for this bet!")
+            action = input('do you wnat continue? (yes/no): ').lower()
+            if action == 'yes':
+                continue
+            else:
+                break
+
+#play game
+main()
